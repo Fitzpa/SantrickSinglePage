2b7867f169793792405de69b592b9892571e717d
unknown <fitz4492@gmail.com>
2019-08-12 14:49:03 -0500

    mobile

--- src/components/Box.js/index.js
+++ src/components/Box.js/index.js
@@ -6,20 +6,21 @@ import burrow from "../../videos/burrow/burrow_compressed.mp4"
 const Box = () => {
   return (
     <div className={boxStyles.box_container}>
+      
       <div className={boxStyles.tile}>
+        <img src={photo_1} alt="one" className={boxStyles.box_img_sm} />
+        <div className={boxStyles.details}>
+          <span className={boxStyles.tileTitle}>Maynards</span>
+          <span className={boxStyles.tileInfo}>Out on the Warf</span>
+        </div>
+      </div>
+      <div id="burrowVideo" className={boxStyles.tile}>
         <video src={burrow} type="video/mp4" loop="true" autoplay="true" className={boxStyles.player_styles}>
           Your Browser Does Not Support This Video
         </video>
         <div className={boxStyles.details}>
           <span className={boxStyles.tileTitle}>The Burrow</span>
           <span className={boxStyles.tileInfo}>Lets throw some axes!</span>
-        </div>
-      </div>
-      <div className={boxStyles.tile}>
-        <img src={photo_1} alt="one" className={boxStyles.box_img_sm} />
-        <div className={boxStyles.details}>
-          <span className={boxStyles.tileTitle}>Maynards</span>
-          <span className={boxStyles.tileInfo}>Out on the Warf</span>
         </div>
       </div>
     </div>
